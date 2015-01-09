b3f15e523def235502dd9df82ad494010cde6a6c
Tobias Rojahn <tr@wlec.ag>
2015-01-09 16:06:47 +0100

    splitted templates

---
+++ install/tpl/footer.php
@@ -0,0 +1,14 @@
+					</div>
+
+					<div class="modal-footer">
+						<button id="migbtn" class="btn btn-danger" onclick="alert('this button dont has any function')">Cancel</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script type="text/javascript" src="install/js/jquery.min.js"></script>
+		<script type="text/javascript" src="install/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="install/js/webcomposer.js"></script>
+	</body>
+</html>
---
+++ install/tpl/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>Installation</title>
+
+	<link rel="stylesheet" href="install/css/bootstrap.min.css" type="text/css">
+	<link rel="stylesheet" href="install/css/bootstrap-theme.min.css" type="text/css">
+	<link rel="stylesheet" href="install/css/webcomposer.css" type="text/css">
+</head>
+<body>
+
+<div class="modal" style="display: block">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title">Installation</h4>
+			</div>
+
+			<div class="modal-body">
